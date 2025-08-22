480cde9b7e342dd783b1945e1825462b09f0749e
HimanshuHegde <himanshuhegde797@gmail.com>
2025-08-22 09:45:34 +0530

    fixed a few bugs during fetching

--- blood-bank/polling.js
+++ blood-bank/polling.js
@@ -1,6 +1,6 @@
 let lastFetched = null;
 const SECRET_KEY = "lasdfaldf234232wqa122fsvsdlfjsdvnsasifjweiojsadlflkasdjflkasjflk32234234edswdsjfflas2 3rsd"; 
-const baseUrl = "https://script.google.com/macros/s/AKfycbzWYYnT46qKH0AxD18VGvi6Yu40JpGEWZ159EieWqvITvHIprl7enxi06sF26KT043F/exec";
+const baseUrl = "https://script.google.com/macros/s/AKfycbzEckJH-sjV_vz299x0TSEAq3HjbQRTpUZ6FzXtz-tV3GCyVJYQX89zlPHocbRslVyv/exec";
 
 async function pollUpdates() {
   let url = `${baseUrl}?key=${SECRET_KEY}`;
@@ -9,6 +9,7 @@ async function pollUpdates() {
   }
 
   try {
+    lastFetched = new Date().toISOString(); // update lastFetched
     const res = await fetch(url);
     const data = await res.json();
     console.log("New/updated rows:", data);
@@ -18,7 +19,6 @@ async function pollUpdates() {
     
     }
 
-    lastFetched = new Date().toISOString(); // update lastFetched
   } catch (err) {
     console.error("Error fetching updates:", err);
   }
